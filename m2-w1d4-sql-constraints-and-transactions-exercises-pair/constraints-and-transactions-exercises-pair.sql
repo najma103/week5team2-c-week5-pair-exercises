93b181e49d630fa06e8c5cf45ae895a0a7dc2a03
Najma Aden <najma.aden@gmail.com>
2017-02-16 15:38:21 -0500

    updated pair exerciases for week 5

--- m2-w1d4-sql-constraints-and-transactions-exercises-pair/constraints-and-transactions-exercises-pair.sql
+++ m2-w1d4-sql-constraints-and-transactions-exercises-pair/constraints-and-transactions-exercises-pair.sql
@@ -11,7 +11,7 @@ VALUES ('Smallville', 'USA', 'Kansas', 45001);
 -- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
 -- percentage of the 'USA' population.
 
-INSERT INTO city (countrycode, language, isofficial, percentage)  
+INSERT INTO countrylanguage(countrycode, language, isofficial, percentage)  
 VALUES ('USA', 'Kryptonese', 'false', 0.0001);
 
 -- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble", change 
@@ -52,11 +52,31 @@ WHERE id = 4080;
 -- (exclusive). 
 -- (590 rows affected)
 
+
+UPDATE countrylanguage
+SET isofficial = CASE WHEN isofficial = 1 THEN 0 ELSE 1 END
+from countrylanguage cl
+join country co on co.code = cl.countrycode
+where co.indepyear >= 1800 and co.indepyear <= 1972
+
+
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
 -- the nearest integer value greater than 0.)
--- (4068 rows affected)
+-- (4079 rows affected)
+
+update city 
+set population = round(population/1000, 1)
+
 
 -- 10. Assuming a country's surfacearea is expressed in miles, convert it to 
 -- meters for all countries where French is spoken by more than 20% of the 
 -- population.
 -- (7 rows affected)
+
+update country 
+set surfacearea = surfacearea*1609
+from country co
+join countrylanguage cl on cl.countrycode = co.code
+where cl.language = 'french' and percentage >=20
+
+
